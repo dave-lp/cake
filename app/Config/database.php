28b9d8e3ac03d7c61bbd78b571928c6ad48b418d
DaveHome <dave@theloanpost.com>
2013-12-17 11:53:47 -0500

    new db to learn

--- app/Config/database.php
+++ app/Config/database.php
@@ -67,7 +67,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
-		'database' => 'cake',
+		'database' => 'learning',
 		'prefix' => '',
 		'encoding' => 'utf8',
 	);
